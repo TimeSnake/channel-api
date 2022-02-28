165672618498facc9936799a2c5dc07729e502d2
funzter <funzter@mail.timesnake.de>
2022-02-28 02:35:55 +0100

    rearranging code

--- src/main/java/de/timesnake/channel/util/message/ChannelDiscordMessage.java
+++ src/main/java/de/timesnake/channel/util/message/ChannelDiscordMessage.java
@@ -25,14 +25,14 @@ public class ChannelDiscordMessage<Value> extends ChannelMessage<String, Value> 
 
         private final Map<String, List<UUID>> uuidsByTeam;
 
-        protected Allocation(String toParse){
+        protected Allocation(String toParse) {
             uuidsByTeam = new HashMap<>();
 
-            for(String teamAllocation : toParse.split(TEAM_DELIMITER)){
+            for (String teamAllocation : toParse.split(TEAM_DELIMITER)) {
                 String[] values = teamAllocation.split(NAME_DELIMITER);
                 List<UUID> uuids = new LinkedList<>();
 
-                for(int i = 1; i < values.length; i++){
+                for (int i = 1; i < values.length; i++) {
                     uuids.add(UUID.fromString(values[i]));
                 }
 
@@ -41,38 +41,38 @@ public class ChannelDiscordMessage<Value> extends ChannelMessage<String, Value> 
             }
         }
 
-        public Allocation(Map<String, List<UUID>> uuidsByTeam){
+        public Allocation(Map<String, List<UUID>> uuidsByTeam) {
             this.uuidsByTeam = uuidsByTeam;
         }
 
 
-        public Map<String, List<UUID>> getAllocation(){
+        public Map<String, List<UUID>> getAllocation() {
             return uuidsByTeam;
         }
 
         @Override
-        public String toString(){
+        public String toString() {
             StringBuilder sb = new StringBuilder();
 
-            for(Map.Entry<String, List<UUID>> entry : uuidsByTeam.entrySet()){
+            for (Map.Entry<String, List<UUID>> entry : uuidsByTeam.entrySet()) {
                 sb.append(entry.getKey()); // Team
 
-                if(!entry.getValue().isEmpty()){
+                if (!entry.getValue().isEmpty()) {
                     sb.append(NAME_DELIMITER);
 
-                    for(UUID player : entry.getValue()){
+                    for (UUID player : entry.getValue()) {
                         sb.append(player.toString());
                         sb.append(NAME_DELIMITER);
                     }
 
-                    sb.deleteCharAt(sb.length()-1); // Remove last NAME_DELIMITER
+                    sb.deleteCharAt(sb.length() - 1); // Remove last NAME_DELIMITER
 
                 }
 
                 sb.append(TEAM_DELIMITER); // End of team
             }
 
-            sb.deleteCharAt(sb.length()-1); // Remove last TEAM_DELIMITER
+            sb.deleteCharAt(sb.length() - 1); // Remove last TEAM_DELIMITER
             return sb.toString();
         }
 
